570781ddccee2490329b22e2f881393f0d2aa2b5
Israel Albarrán <76792382+Isra48@users.noreply.github.com>
2022-06-28 17:23:35 -0500

    navbar1

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,54 @@
 import React from "react";
 import "./NavBar.css";
+import logoch from "../assets/ch.png";
+import Cw from "./cartwidget";
+import hamburguernav from "../assets/hamburguernavicon.png";
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <div className="containerlogo">
-        <a href="" className="logonavbar">
-          {" "}
-          logo{" "}
-        </a>
+    <div className="masternavbar">
+      <div className="navbar">
+        <div className="containerlogo">
+          <a href="" className="logonavbar">
+            {" "}
+            <img className="logoch" src={logoch} alt="Logo Coder House" />{" "}
+          </a>
+        </div>
+
+        <div className="itemsnavbar">
+          <a href="" className="textnavbar">
+            {" "}
+            Inicio{" "}
+          </a>
+          <a href="" className="textnavbar">
+            Shop
+          </a>
+          <a href="" className="textnavbar">
+            Nosotros
+          </a>
+          <a href="" className="textnavbar">
+            Contacto
+          </a>
+          <Cw />
+        </div>
       </div>
-      <div className="componentsnavbar">
-        <a href="" className="textnavbar">
-          {" "}
-          Shop{" "}
-        </a>
-        <a href="" className="textnavbar">
-          {" "}
-          nosotros{" "}
-        </a>
+
+      {/* Aqui inicia el menu responsive */}
+      <div className="navbar2">
+        <div className="containerlogo">
+          <a href="" className="logonavbar">
+            {" "}
+            <img className="logoch" src={logoch} alt="Logo Coder House" />{" "}
+          </a>
+        </div>
+        <div className="hamburguer-bag">
+          <Cw />
+          <img
+            className="hamburguernav"
+            src={hamburguernav}
+            alt="Menu de hamburguesa"
+          />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/itemListContainer.js
@@ -0,0 +1,12 @@
+import "./itemlistContainer.css";
+
+function itemlistContainer() {
+  return (
+    <div className="Cards">
+      <div className="card">1 </div>
+      <div className="card"> 2</div>
+      <div className="card">3 </div>
+    </div>
+  );
+}
+export default itemlistContainer;
