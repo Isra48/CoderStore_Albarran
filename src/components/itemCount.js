136b70099f764c3f4ea2cd8dc77b5d344471bb68
Israel Albarrán <76792382+Isra48@users.noreply.github.com>
2022-06-30 00:01:37 -0500

    items Count added

---
+++ src/components/itemCount.js
@@ -0,0 +1,13 @@
+import "./itemCount.css";
+
+function itemCount() {
+  return (
+    <div className="itemCount">
+      <button> -</button>
+      <div className=""> 9</div>
+
+      <button>+ </button>
+    </div>
+  );
+}
+export default itemCount;
--- src/components/itemListContainer.js
+++ src/components/itemListContainer.js
@@ -1,11 +1,21 @@
 import "./itemListContainer.css";
+import ItemCount from "./itemCount";
 
 function itemlistContainer(props) {
   return (
     <div className="Cards">
-      <div className="Card">1 {props.message}</div>
-      <div className="Card">2{props.message}</div>
-      <div className="Card">3 {props.message} </div>
+      <div className="Card">
+        1 {props.message}
+        <ItemCount />
+      </div>
+      <div className="Card">
+        2{props.message}
+        <ItemCount />
+      </div>
+      <div className="Card">
+        3 {props.message}
+        <ItemCount />
+      </div>
     </div>
   );
 }
