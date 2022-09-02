dc8167a97394ff8b29fb51e83f82a508da255a55
Israel Albarran <israelalbarran48@gmail.com>
2022-09-02 00:07:27 -0500

    item count logic

--- src/components/itemCount.js
+++ src/components/itemCount.js
@@ -1,13 +1,30 @@
+import React from "react";
 import "./itemCount.css";
+import { useState } from "react";
 
-function itemCount() {
+function ItemCount() {
+ const [count, setCount] = useState(0)
+ 
   return (
     <div className="itemCount">
-      <button className="btnl"> - </button>
-      <div className="contador"> 9</div>
+      <button className="btnl" onClick={() => {
+        if (count >  0) {
+          setCount(count - 1) ;
 
-      <button className="btnr"> + </button>
+        }
+      else{
+        setCount (0);
+
+      }
+      
+        
+      }
+
+      } > - </button>
+      <div className="contador"> {count}</div>
+
+      <button className="btnr" onClick={() => setCount(count + 1)}>  + </button>
     </div>
   );
 }
-export default itemCount;
+export default ItemCount;
